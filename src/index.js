95adca26d119c480c42bbf4461e4aa6c34773252
Volodymyr Vasylkivskyi <vasilksvskij@gmail.com>
2023-01-09 06:58:45 +0200

    feat: add my code to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  let arr = String(n).split('');
+  let arrReverse = [];
+
+  if (isNaN(arr[0])) {
+    for (i = arr.length - 1; i > 0; i--) {
+      arrReverse.push(arr[i]);
+    }
+  } else {
+    for (i = arr.length - 1; i >= 0; i--) {
+      arrReverse.push(arr[i]);
+    }
+  }
+
+  return arrReverse.join('');
 }
